aadd6660948a002db2ee4792d96b608f7e8370a6
Mini Kaushik <55741750+mini2580@users.noreply.github.com>
2021-03-16 11:56:27 +0000

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,46 @@
+﻿
+using System; 
+
+class Account{
+
+    private string name; // instance variable
+    private double balance; //instance variable
+    public static string companyName = "Learn Prodigy"; // static member ; one copy through out ; no seperate copies as per the instance 
+
+    // property for companyName
+
+    public string CompanyName
+    {
+        get { return companyName; }
+    }
+
+    //Set method - will set the value 
+    public void SetName(string accountName)
+    {
+        name = accountName;
+    }// set method end
+
+    // Get method will return the value to the client
+    public string GetName()
+    {
+        return name;
+    }// Get Method end
+
+    public void SetBalance(double bal)
+    {
+        if (bal >= 0)
+        {
+            balance = bal;
+        }
+        else
+        {
+            Console.WriteLine("Balance must be a positive number");
+        }
+    }// end of SetBAlance Method 
+
+    public double GetBalance()
+    {
+
+        return balance;
+    }
+}
---
+++ AccountTest.cs
@@ -0,0 +1,39 @@
+﻿using System; // directive
+
+class AccountTest
+{
+    static void Main()
+    {
+
+        // ceate Account object 
+
+        Account myAccount = new Account();
+
+        //  myAccount.name = "Mini";  tried to access the private variable ; will give an error 
+
+        Console.WriteLine($"Initial Name is: {myAccount.GetName()}");
+        Console.WriteLine($"Initial Balance is: {myAccount.GetBalance()}");
+
+        Console.Write("Enter the Name: ");
+        string theName = Console.ReadLine();
+        myAccount.SetName(theName);
+        Console.WriteLine();
+        Console.Write("Enter the Balance: ");
+         double theBalance = Double.Parse(Console.ReadLine());
+        myAccount.SetBalance(theBalance);
+        DisplayAccount(myAccount);
+
+      //  Console.WriteLine($"The name of the Account is : {myAccount.GetName()}");
+
+        //Console.WriteLine($"The Balance of the Account is : {myAccount.GetBalance()}");
+
+    }//end of Main class
+
+    static void DisplayAccount(Account acct)
+    {
+        Console.WriteLine($"The company {Account.companyName} has the Account  : {acct.GetName()}");// accessing the static member using class name 
+
+        Console.WriteLine($"The Balance of the Account is : {acct.GetBalance()}");
+
+    }
+}
